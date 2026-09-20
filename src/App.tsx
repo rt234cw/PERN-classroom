e8c015e91d2ae3f7ed23a1eb9c84dbdd37ad01e2
yt <113787435+rt234cw@users.noreply.github.com>
2026-09-20 16:07:05 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { BookOpen, Home } from "lucide-react";
 
 import { BrowserRouter, Route, Routes, Outlet } from "react-router";
 import routerProvider, {
@@ -15,9 +16,9 @@ import routerProvider, {
   DocumentTitleHandler,
 } from "@refinedev/react-router";
 import { dataProvider } from "./providers/data";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { ForgotPassword } from "./pages/forgot-password";
+// import { Login } from "./pages/login";
+// import { Register } from "./pages/register";
+// import { ForgotPassword } from "./pages/forgot-password";
 import { ErrorComponent } from "./components/refine-ui/layout/error-component";
 import { Layout } from "./components/refine-ui/layout/layout";
 import { Header } from "./components/refine-ui/layout/header";
@@ -25,11 +26,13 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import "./App.css";
+import Dashboard from "./pages/dashboard";
+import SubjectList from "./pages/subjects/list";
+import SubjectsCreate from "./pages/subjects/create";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -37,6 +40,19 @@ function App() {
               dataProvider={dataProvider}
               notificationProvider={useNotificationProvider()}
               routerProvider={routerProvider}
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: { label: "Home", icon: <Home /> },
+                },
+                {
+                  name: "subjects",
+                  list: "/subjects",
+                  create: '/subjects/create',
+                  meta: { label: "Subjects", icon: <BookOpen /> },
+                },
+              ]}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
@@ -44,7 +60,18 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+
+                <Route path="/" element={<Dashboard />} />
+                <Route path="subjects">
+                  <Route index element={<SubjectList />} />
+                  <Route path="create" element={<SubjectsCreate />}/>
+                </Route>
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
---
+++ src/pages/subjects/create.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubjectsCreate = () => {
+  return (
+    <div>create</div>
+  )
+}
+
+export default SubjectsCreate
